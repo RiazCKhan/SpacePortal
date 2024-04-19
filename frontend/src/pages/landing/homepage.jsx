4c12a20b8de5f00153300664c3bc57404b6b955f
RiazK <RiazCKhan@gmail.com>
2024-04-19 17:44:27 -0400

    add mui icons

---
+++ frontend/src/pages/landing/homepage.jsx
@@ -0,0 +1,10 @@
+import { CssBaseline } from "@mui/material";
+
+export default function Homepage() {
+  return (
+    <div className="landing">
+      Pending...
+      <CssBaseline />
+    </div>
+  );
+}
