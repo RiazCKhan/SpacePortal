0e50ebf5f387b189de9293598b2c39e514d962fc
RiazK <RiazCKhan@gmail.com>
2024-04-24 16:54:15 -0400

    add static file structure

---
+++ frontend/src/static/spacePlaces.js
@@ -0,0 +1,18 @@
+const spacePlaces = [
+  {
+    id: 1,
+    title: "temp",
+    description: "temp",
+    imageUrl: "temp",
+    time: 1500,
+  },
+  {
+    id: 2,
+    title: "temp",
+    description: "temp",
+    imageUrl: "temp",
+    time: 1500,
+  }
+]
+
+export default spacePlaces
