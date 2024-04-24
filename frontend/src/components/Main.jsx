228a0295ed5d1059b207d92f5d42ef075ece6179
RiazK <RiazCKhan@gmail.com>
2024-04-24 16:54:01 -0400

    add map prop component

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,9 +1,30 @@
 import ImageCard from "./ImageCard";
+import spacePlaces from "../static/spacePlaces";
 
 export default function Main() {
+  const spaceProps = spacePlaces.map((item) => {
+    return (
+      <ImageCard
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        description={item.description}
+        imageUrl={item.imageUrl}
+        time={item.time}
+      />
+    );
+  });
+
   return (
-    <div style={{ minHeight: "90vh" }}>
-      <ImageCard />
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {spaceProps}
     </div>
   );
 }
